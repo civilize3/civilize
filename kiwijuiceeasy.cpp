363de078cc078f28f21e3b90fe4df68ab09d8494
civilize <59631649+civilize3@users.noreply.github.com>
2020-01-14 08:59:47 +0900

    Update kiwijuiceeasy.cpp
    removed main function

--- kiwijuiceeasy.cpp
+++ kiwijuiceeasy.cpp
@@ -20,25 +20,3 @@ vector<int> KiwiJuiceEasy::thePouring(vector<int> capacities, vector<int> bottle
     }
     return bottles;
 }
-
-int main() {
-    int cc[] = {700000,800000,900000,1000000};
-    int bot[] = {478478,478478,478478,478478};
-    int f[] = {2,3,2,0,1};
-    int t[] = {0,1,1,3,2};
-    vector<int> cap(cc, cc + sizeof(cc) / sizeof(int));
-    vector<int> bottles(bot, bot + sizeof(bot) / sizeof(int));
-    vector<int> from(f, f + sizeof(f) / sizeof(int));
-    vector<int> to(t, t + sizeof(t) / sizeof(int));
-    vector<int> result;
-
-    //cap.push_back(20);
-
-    KiwiJuiceEasy kje;
-    result = kje.thePouring(cap,bottles,from,to);
-
-    for (vector<int>::iterator i = result.begin() ; i != result.end() ; ++i)
-        cout << *i << endl;
-
-    return 0;
-}
