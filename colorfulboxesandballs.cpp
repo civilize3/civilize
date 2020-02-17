3c2030a82005c50bb30ac8825f8b3ac3b8a40771
civilize <59631649+civilize3@users.noreply.github.com>
2020-02-17 23:37:19 +0900

    Update colorfulboxesandballs.cpp

--- colorfulboxesandballs.cpp
+++ colorfulboxesandballs.cpp
@@ -9,16 +9,3 @@ public:
 int ColorfulBoxesAndBalls::getMaximum(int numRed, int numBlue, int onlyRed, int onlyBlue, int bothColors) {
     return max((numRed * onlyRed + numBlue * onlyBlue), numRed > numBlue ? (2 * numBlue * bothColors + (numRed - numBlue) * onlyRed) : (2 * numRed * bothColors + (numBlue - numRed) * onlyBlue));
 }
-
-int main()
-{
-    ColorfulBoxesAndBalls cbab;
-    cout << cbab.getMaximum(2,3,100,400,200) << endl;
-    cout << cbab.getMaximum(2,3,100,400,300) << endl;
-    cout << cbab.getMaximum(5,5,464,464,464) << endl;
-    cout << cbab.getMaximum(1,4,20,-30,-10) << endl;
-    cout << cbab.getMaximum(9,1,-1,-10,4) << endl;
-
-    return 0;
-}
-
