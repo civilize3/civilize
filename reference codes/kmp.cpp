2f8e9718e43e07405e753808a5323f8fca958a74
root <civilize@naver.com>
2020-04-02 08:29:57 +0900

    Added KMP algorithm for fast string find

---
+++ reference codes/kmp.cpp
@@ -0,0 +1,34 @@
+#include <cstdio>
+#include <vector>
+#include <string>
+#include <iostream>
+using namespace std;
+
+vector<int> getPi(string target) {
+    vector<int> pi;
+    unsigned int len = target.length();
+    pi.push_back(0);
+    for (unsigned int i = 1, m = 0 ; i < len ; i++) {
+        while (m > 0 && target[i] != target[m]) m = pi[m - 1];
+        if (target[i] == target[m]) pi[i] = ++m;
+        pi.push_back(m);
+    }
+    return pi;
+}
+
+int main() {
+    string src, target;
+    cin >> src >> target;
+    vector<int> pi;
+
+    pi = getPi(target);
+
+    for (unsigned int i = 0, m = 0 ; i < src.length() ; i++) {
+        while (m > 0 && src[i] != target[m]) m = pi[m - 1];
+        if (src[i] == target[m]) ++m;
+        if (m >= target.length()) { printf("%d\n", i - m + 1); break; }
+        else if (i == src.length() - 1) printf("-1\n");
+    }
+
+    return 0;
+}
